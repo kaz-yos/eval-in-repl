36eff6bf5ba2ef6b99479c73c483751b564dae84
kaz-yos <kazky@mac.com>
2014-06-29 15:27:37 +0900

    Use "%VERSION%" in -pkg.el

--- eval-in-repl-pkg.el
+++ eval-in-repl-pkg.el
@@ -1,3 +1,2 @@
-(define-package "eval-in-repl"
-  "0.1.0" "Consistent eval interface for various REPLs"
+(define-package "eval-in-repl" "%VERSION%" "Consistent eval interface for various REPLs"
   '((dash "0.0.0")))
