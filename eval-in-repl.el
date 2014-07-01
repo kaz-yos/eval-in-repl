dc6db92f82d9216f820a7c7b862cca4447a362ad
kaz-yos <kazky@mac.com>
2014-07-01 11:15:47 +0900

    Change description in main file
    The Commentary in the main file is used by package.el

--- eval-in-repl.el
+++ eval-in-repl.el
@@ -5,7 +5,7 @@
 ;; Author: Kazuki YOSHIDA <kazukiyoshida@mail.harvard.edu>
 ;; Keywords: tools, convenience
 ;; URL: https://github.com/kaz-yos/eval-in-repl
-;; Version: 0.1.0
+;; Version: 0.1.1
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -30,7 +30,8 @@
 ;; Emacs Speaks Statistics (ESS) package has a nice function called
 ;; ess-eval-region-or-line-and-step, which is assigned to C-RET.
 ;; This function sends a line or a selected region to the corresponding
-;; shell (R, Julia, Stata, etc) visibly. It also start up a shell if there is none.
+;; shell (R, Julia, Stata, etc) visibly. It also start up a shell if
+;; there is none.
 ;;
 ;; This package along with a REPL/shell specific packages implement similar
 ;; work flow for various REPLs.
@@ -38,15 +39,19 @@
 ;; This file alone is not functional.
 ;; Also require the following depending on your needs.
 ;;
-;; eval-in-repl-ielm.el	   for Emacs Lisp (via ielm)
+;; eval-in-repl-ielm.el    for Emacs Lisp (via ielm)
 ;; eval-in-repl-cider.el   for Clojure (via cider.el)
 ;; eval-in-repl-slime.el   for SLIME (via slime.el)
-;; eval-in-repl-scheme.el  for Scheme (if used through scheme.el and cmuscheme.el)
+;; eval-in-repl-scheme.el  for Scheme (via scheme.el and cmuscheme.el)
 ;; eval-in-repl-python.el  for Python (via python.el)
 ;; eval-in-repl-shell.el   for shell mode (via essh.el)
 ;;
 ;; See below for installation and an configuration example.
 ;; https://github.com/kaz-yos/eval-in-repl/
+;;
+;; Version history
+;; 0.1.1 Delete excess autoload macros
+;; 0.1.0 Release
 
 
 ;;; Code:
