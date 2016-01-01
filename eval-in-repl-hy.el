154097403e2486eaa2d6d679f6b671f2cc92e524
kaz-yos <kazukiyoshida@mail.harvard.edu>
2016-01-01 17:43:12 -0500

    Add specialized inferior-hy command
    Can invoke hy at any window

--- eval-in-repl-hy.el
+++ eval-in-repl-hy.el
@@ -53,6 +53,13 @@
   "Send expression to *inferior-lisp* and have it evaluated.")
 
 
+;;; inferior-hy
+(defun eir-inferior-hy ()
+  "Call inferior hy process at any buffer"
+  (interactive)
+  (inferior-lisp hy-mode-inferior-lisp-command))
+
+
 ;;; eir-eval-in-hy
 ;;;###autoload
 (defun eir-eval-in-hy ()
@@ -62,7 +69,7 @@
    ;; repl-buffer-regexp
    "\\*inferior-lisp\\*"
    ;; fun-repl-start
-   'inferior-lisp
+   'eir-inferior-hy
    ;; fun-repl-send
    'eir-send-to-hy
    ;; defun-string
@@ -71,19 +78,3 @@
 
 (provide 'eval-in-repl-hy)
 ;;; eval-in-repl-hy.el ends here
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
