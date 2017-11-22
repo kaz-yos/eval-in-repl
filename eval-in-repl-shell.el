6fad41bccb2fe7bb05273ebf887302fc3d825e28
Terje Larsen <terlar@gmail.com>
2017-11-22 13:27:40 +0100

    Add support for different shells in sh-mode
    This launches a corresponding shell for the one specified in the script.

--- eval-in-repl-shell.el
+++ eval-in-repl-shell.el
@@ -1,11 +1,11 @@
 ;;; eval-in-repl-shell.el --- ESS-like eval for shell  -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2014-  Kazuki YOSHIDA
+;; Copyright (C) 2017  Kazuki YOSHIDA, Terje Larsen
 
-;; Author: Kazuki YOSHIDA <kazukiyoshida@mail.harvard.edu>
+;; Author: Kazuki YOSHIDA <kazukiyoshida@mail.harvard.edu>, Terje Larsen <terlar@gmail.com>
 ;; Keywords: tools, convenience
 ;; URL: https://github.com/kaz-yos/eval-in-repl
-;; Version: 0.9.4
+;; Version: 0.9.6
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -23,7 +23,7 @@
 
 ;;; Commentary:
 
-;; essh.el-specific file for eval-in-repl
+;; sh-script.el-specific file for eval-in-repl
 ;; See below for configuration
 ;; https://github.com/kaz-yos/eval-in-repl/
 
@@ -33,36 +33,50 @@
 ;;;
 ;;; Require dependencies
 (require 'eval-in-repl)
+(require 'sh-script)
+(require 'comint)
 
 
 ;;;
 ;;; SHELL RELATED
-;;
+
+;;;###autoload
+(defun eir-shell-repl ()
+  "Start or switch to `sh-shell' REPL buffer."
+  (interactive)
+  (let* ((dest-sh (symbol-name sh-shell))
+         (buffer-name (format "*shell [%s]*" dest-sh))
+         ;; This variable is read by shell function and determines which shell
+         ;; is launched.
+         (explicit-shell-file-name dest-sh))
+    (shell buffer-name)))
+
 ;;; eir-send-to-shell
 (defalias 'eir-send-to-shell
   (apply-partially 'eir-send-to-repl
                    ;; fun-change-to-repl
-                   #'(lambda () (switch-to-buffer-other-window "*shell*"))
+                   #'eir-shell-repl
                    ;; fun-execute
                    #'comint-send-input)
   "Send expression to *shell* and have it evaluated.")
 
-
 ;;; eir-eval-in-shell
 ;;;###autoload
 (defun eir-eval-in-shell ()
-  "eval-in-repl for shell."
+  "Provides eval-in-repl for `sh-script' files."
   (interactive)
   ;; Define local variables
   (let* (;; Save current point
-	 (initial-point (point)))
+        (initial-point (point))
+        ;; Shell buffer
+        (buffer-name (format "*shell [%s]*" (symbol-name sh-shell))))
     ;;
-    (eir-repl-start "\\*shell\\*" #'shell t)
+    (eir-repl-start (regexp-quote buffer-name) #'eir-shell-repl t)
 
     ;; Check if selection is present
     (if (and transient-mark-mode mark-active)
-	;; If selected, send region
-	(eir-send-to-shell (buffer-substring-no-properties (point) (mark)))
+       ;; If selected, send region
+       (eir-send-to-shell (buffer-substring-no-properties (point) (mark)))
 
       ;; If not selected, do all the following
       ;; Move to the beginning of line
@@ -73,9 +87,9 @@
       (end-of-line)
       ;; Send region if not empty
       (if (not (equal (point) (mark)))
-	  (eir-send-to-shell (buffer-substring-no-properties (point) (mark)))
-	;; If empty, deselect region
-	(setq mark-active nil))
+         (eir-send-to-shell (buffer-substring-no-properties (point) (mark)))
+       ;; If empty, deselect region
+       (setq mark-active nil))
 
       ;; Move to the next statement code if jumping
       (if eir-jump-after-eval
@@ -86,4 +100,3 @@
 
 (provide 'eval-in-repl-shell)
 ;;; eval-in-repl-shell.el ends here
-
